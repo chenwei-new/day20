396cc2057400577b3d322d03c76a65ffe1b27def
chenwei <863755885@qq.com>
2021-09-03 14:13:07 +0800

    教师实体新增了tid属性

--- src/com/ujiuye/entity/Teacher.java
+++ src/com/ujiuye/entity/Teacher.java
@@ -1,5 +1,20 @@
 package com.ujiuye.entity;
 
 public class Teacher {
+    private int tid;
 
+    public Teacher() {
+    }
+
+    public Teacher(int tid) {
+        this.tid = tid;
+    }
+
+    public int getTid() {
+        return tid;
+    }
+
+    public void setTid(int tid) {
+        this.tid = tid;
+    }
 }
